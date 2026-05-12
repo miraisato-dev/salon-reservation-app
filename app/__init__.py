ca5bc73ada0d4d7823841831a735fc88a3908266
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-05-12 22:25:32 +0900

    stylistsのルーティングを切り分け

--- app/__init__.py
+++ app/__init__.py
@@ -67,13 +67,14 @@ def create_app():
     from app.auth.routes import auth_bp
     from app.customers.routes import customers_bp
     from app.reservations.routes import reservations_bp
+    from app.stylists.routes import stylists_bp
 
     # blueprint register
     app.register_blueprint(auth_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(customers_bp)
     app.register_blueprint(reservations_bp)
+    app.register_blueprint(stylists_bp)
 
     return app
-
 
